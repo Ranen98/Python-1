2e025fd12a675eb3a137c45720f22aa491190984
Ranen98 <908647907@foxmail.com>
2019-03-24 22:11:20 +0800

    2019/3/24/22:11

--- 项目/6、进程线程/2、线程/2、多线程共享全局变量.py
+++ 项目/6、进程线程/2、线程/2、多线程共享全局变量.py
@@ -1,18 +1,18 @@
 import threading
-import time
 
-def run():
-    print("子线程<%s>启动" % (threading.current_thread().name))
-    time.sleep(2)
-    print("这是子线程<%s>" % (threading.current_thread().name))
-    time.sleep(2)
-    print("子线程<%s>结束" % (threading.current_thread().name))
+num = 0
+def run(n):
+    global num
+    for i in range(10000000):
+        num = num + n
+        num = num - n
+    print(num)
 
 if __name__ == '__main__':
     print("主线程启动")
 
-    t1 = threading.Thread(target=run)
-    t2 = threading.Thread(target=run)
+    t1 = threading.Thread(target=run, args=(4,))
+    t2 = threading.Thread(target=run, args=(8,))
     t1.start()
     t2.start()
     t1.join()
